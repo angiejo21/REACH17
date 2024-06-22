e6b7c3f11907c5012d8d9502c634c4052aabd78c
Angela Bellò <angela.bello90@gmail.com>
2024-06-22 18:02:10 +0200

    added basic error handling outside express

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 
+//Error in Sync code - In case of errors outside of express
+process.on("uncaughtException", (err) => {
+  console.log("💥UNCAUGHT EXCEPTION! Shutting down...");
+  console.log(`${err.name}: ${err.message}`);
+  process.exit(1); //0 - success 1 - uncalled exeption
+});
+
 dotenv.config({ path: "./config.env" });
 
 import app from "./app.js";
@@ -17,4 +24,14 @@ console.log("Connected to database");
 
 const server = app.listen(port, () => {
   console.log(`App running on port ${port} in ${process.env.NODE_ENV}`);
+});
+
+//Error in Async code - In case of a rejected promise outside express(e.g. database failed to connect)
+process.on("unhandledRejection", (err) => {
+  console.log("💥UNHANDLED REJECTION! Shutting down...");
+  console.log(`${err.name}: ${err.message}`);
+  server.close(() => {
+    //Close gracefully and finish pending requests
+    process.exit(1);
+  });
 });
